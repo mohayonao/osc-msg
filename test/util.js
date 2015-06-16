7e22cb3f1660d79f9914323aa15c3eba204bac6a
mohayonao <mohayonao@gmail.com>
2015-06-16 13:09:55 +0900

    fix tests

--- test/util.js
+++ test/util.js
@@ -17,86 +17,98 @@ describe("util", function() {
   });
 
   describe(".isInteger", function() {
-    assert(util.isInteger(-1) === true);
-    assert(util.isInteger(10) === true);
-    assert(util.isInteger(1.5) === false);
-    assert(util.isInteger(Infinity) === false);
-    assert(util.isInteger(NaN) === false);
-    assert(util.isInteger(true) === false);
-    assert(util.isInteger(false) === false);
-    assert(util.isInteger(null) === false);
-    assert(util.isInteger("0") === false);
-    assert(util.isInteger(new Buffer(0)) === false);
-    assert(util.isInteger(new Uint8Array(0).buffer) === false);
+    it("(value: any): boolean", function() {
+      assert(util.isInteger(-1) === true);
+      assert(util.isInteger(10) === true);
+      assert(util.isInteger(1.5) === false);
+      assert(util.isInteger(Infinity) === false);
+      assert(util.isInteger(NaN) === false);
+      assert(util.isInteger(true) === false);
+      assert(util.isInteger(false) === false);
+      assert(util.isInteger(null) === false);
+      assert(util.isInteger("0") === false);
+      assert(util.isInteger(new Buffer(0)) === false);
+      assert(util.isInteger(new Uint8Array(0).buffer) === false);
+    });
   });
 
   describe(".isFloat", function() {
-    assert(util.isFloat(-1) === true);
-    assert(util.isFloat(10) === true);
-    assert(util.isFloat(1.5) === true);
-    assert(util.isFloat(Infinity) === true);
-    assert(util.isFloat(NaN) === false);
-    assert(util.isFloat(true) === false);
-    assert(util.isFloat(false) === false);
-    assert(util.isFloat(null) === false);
-    assert(util.isFloat("0") === false);
-    assert(util.isFloat(new Buffer(0)) === false);
-    assert(util.isFloat(new Uint8Array(0).buffer) === false);
+    it("(value: any): boolean", function() {
+      assert(util.isFloat(-1) === true);
+      assert(util.isFloat(10) === true);
+      assert(util.isFloat(1.5) === true);
+      assert(util.isFloat(Infinity) === true);
+      assert(util.isFloat(NaN) === false);
+      assert(util.isFloat(true) === false);
+      assert(util.isFloat(false) === false);
+      assert(util.isFloat(null) === false);
+      assert(util.isFloat("0") === false);
+      assert(util.isFloat(new Buffer(0)) === false);
+      assert(util.isFloat(new Uint8Array(0).buffer) === false);
+    });
   });
 
   describe(".isString", function() {
-    assert(util.isString(-1) === false);
-    assert(util.isString(10) === false);
-    assert(util.isString(1.5) === false);
-    assert(util.isString(Infinity) === false);
-    assert(util.isString(NaN) === false);
-    assert(util.isString(true) === false);
-    assert(util.isString(false) === false);
-    assert(util.isString(null) === false);
-    assert(util.isString("0") === true);
-    assert(util.isString(new Buffer(0)) === false);
-    assert(util.isString(new Uint8Array(0).buffer) === false);
+    it("(value: any): boolean", function() {
+      assert(util.isString(-1) === false);
+      assert(util.isString(10) === false);
+      assert(util.isString(1.5) === false);
+      assert(util.isString(Infinity) === false);
+      assert(util.isString(NaN) === false);
+      assert(util.isString(true) === false);
+      assert(util.isString(false) === false);
+      assert(util.isString(null) === false);
+      assert(util.isString("0") === true);
+      assert(util.isString(new Buffer(0)) === false);
+      assert(util.isString(new Uint8Array(0).buffer) === false);
+    });
   });
 
   describe(".isBlob", function() {
-    assert(util.isBlob(-1) === false);
-    assert(util.isBlob(10) === false);
-    assert(util.isBlob(1.5) === false);
-    assert(util.isBlob(Infinity) === false);
-    assert(util.isBlob(NaN) === false);
-    assert(util.isBlob(true) === false);
-    assert(util.isBlob(false) === false);
-    assert(util.isBlob(null) === false);
-    assert(util.isBlob("0") === false);
-    assert(util.isBlob(new Buffer(0)) === true);
-    assert(util.isBlob(new Uint8Array(0).buffer) === true);
+    it("(value: any): boolean", function() {
+      assert(util.isBlob(-1) === false);
+      assert(util.isBlob(10) === false);
+      assert(util.isBlob(1.5) === false);
+      assert(util.isBlob(Infinity) === false);
+      assert(util.isBlob(NaN) === false);
+      assert(util.isBlob(true) === false);
+      assert(util.isBlob(false) === false);
+      assert(util.isBlob(null) === false);
+      assert(util.isBlob("0") === false);
+      assert(util.isBlob(new Buffer(0)) === true);
+      assert(util.isBlob(new Uint8Array(0).buffer) === true);
+    });
   });
 
   describe(".isTimetag", function() {
-    assert(util.isTimetag(-1) === false);
-    assert(util.isTimetag(10) === true);
-    assert(util.isTimetag(1.5) === false);
-    assert(util.isTimetag(Infinity) === false);
-    assert(util.isTimetag(NaN) === false);
-    assert(util.isTimetag(true) === false);
-    assert(util.isTimetag(false) === false);
-    assert(util.isTimetag(null) === false);
-    assert(util.isTimetag("0") === false);
-    assert(util.isTimetag(new Buffer(0)) === false);
-    assert(util.isTimetag(new Uint8Array(0).buffer) === false);
+    it("(value: any): boolean", function() {
+      assert(util.isTimetag(-1) === false);
+      assert(util.isTimetag(10) === true);
+      assert(util.isTimetag(1.5) === false);
+      assert(util.isTimetag(Infinity) === false);
+      assert(util.isTimetag(NaN) === false);
+      assert(util.isTimetag(true) === false);
+      assert(util.isTimetag(false) === false);
+      assert(util.isTimetag(null) === false);
+      assert(util.isTimetag("0") === false);
+      assert(util.isTimetag(new Buffer(0)) === false);
+      assert(util.isTimetag(new Uint8Array(0).buffer) === false);
+    });
   });
 
   describe(".isDouble", function() {
-    assert(util.isDouble(-1) === true);
-    assert(util.isDouble(10) === true);
-    assert(util.isDouble(1.5) === true);
-    assert(util.isDouble(Infinity) === true);
-    assert(util.isDouble(NaN) === false);
-    assert(util.isDouble(true) === false);
-    assert(util.isDouble(false) === false);
-    assert(util.isDouble(null) === false);
-    assert(util.isDouble("0") === false);
-    assert(util.isDouble(new Buffer(0)) === false);
-    assert(util.isDouble(new Uint8Array(0).buffer) === false);
+    it("(value: any): boolean", function() {
+      assert(util.isDouble(-1) === true);
+      assert(util.isDouble(10) === true);
+      assert(util.isDouble(1.5) === true);
+      assert(util.isDouble(Infinity) === true);
+      assert(util.isDouble(NaN) === false);
+      assert(util.isDouble(true) === false);
+      assert(util.isDouble(false) === false);
+      assert(util.isDouble(null) === false);
+      assert(util.isDouble("0") === false);
+      assert(util.isDouble(new Buffer(0)) === false);
+      assert(util.isDouble(new Uint8Array(0).buffer) === false);
+    });
   });
 });
