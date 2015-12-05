cb66078d0f1911dc523c2afd9385d8121b3aec06
mohayonao <mohayonao@gmail.com>
2015-12-05 15:04:09 +0900

    update api interface

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import OSCElement from "./OSCElement";
-import OSCMessage from "./OSCMessage";
-import OSCBundle from "./OSCBundle";
+import decode from "./decode";
+import encode from "./encode";
 
 export default {
-  OSCMessage,
-  OSCBundle,
-  fromBuffer: buffer => OSCElement.fromBuffer(buffer).toObject(),
-  toBuffer: obj => OSCElement.fromObject(obj).toBuffer(),
+  decode,
+  encode,
+  fromBuffer: decode,
+  fromObject: encode,
+  toBuffer: encode,
+  toObject: decode,
 };
